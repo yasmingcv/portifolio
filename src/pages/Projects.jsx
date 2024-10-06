462affdd756dcfef9fa8b9b4feb671109b50a76e
Yasmin Gonçalves <yasmingoncalves5421@gmail.com>
2024-10-06 13:40:53 -0300

    add: projects data

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,40 +1,27 @@
+import { useEffect, useState } from "react";
 import CardProject from "../components/CardProject";
 
 export default function Projects() {
+  const [projects, setProjects] = useState([]);
+  useEffect(() => {
+    fetch("/src/assets/data.json")
+      .then((response) => response.json())
+      .then((jsonData) => setProjects(jsonData))
+      .catch((err) => console.log("Erro ao carregar projetos", err));
+  }, [])
+  
   return (
     <div className="text-white flex flex-col items-center w-full h-full gap-24">
       <h3 className="font-bold text-6xl text-white">Projetos</h3>
       <div className=" w-fit flex flex-wrap gap-4 justify-center">
-        <CardProject
-          name="Projeto1"
-          image="https://imgv3.fotor.com/images/side/astronaut-in-space-looking-up-at-the-stars-generated-by-Fotor-AI.jpg"
-          link="https://www.google.com"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sodales dolor non urna condimentum, finibus ornare massa congue. Donec id turpis sapien."
-        />
-        <CardProject
-          name="Projeto1"
-          image="https://imgv3.fotor.com/images/side/astronaut-in-space-looking-up-at-the-stars-generated-by-Fotor-AI.jpg"
-          link="https://www.google.com"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sodales dolor non urna condimentum, finibus ornare massa congue. Donec id turpis sapien."
-        />
-        <CardProject
-          name="Projeto1"
-          image="https://imgv3.fotor.com/images/side/astronaut-in-space-looking-up-at-the-stars-generated-by-Fotor-AI.jpg"
-          link="https://www.google.com"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sodales dolor non urna condimentum, finibus ornare massa congue. Donec id turpis sapien."
-        />
-        <CardProject
-          name="Projeto1"
-          image="https://imgv3.fotor.com/images/side/astronaut-in-space-looking-up-at-the-stars-generated-by-Fotor-AI.jpg"
-          link="https://www.google.com"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sodales dolor non urna condimentum, finibus ornare massa congue. Donec id turpis sapien."
-        />
-        <CardProject
-          name="Projeto1"
-          image="https://imgv3.fotor.com/images/side/astronaut-in-space-looking-up-at-the-stars-generated-by-Fotor-AI.jpg"
-          link="https://www.google.com"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sodales dolor non urna condimentum, finibus ornare massa congue. Donec id turpis sapien."
-        />
+        {projects.map((project) => {
+          return <CardProject
+            name={project.name}
+            image={project.image}
+            link={project.link}
+            description={project.description}
+          />
+        })}
       </div>
     </div>
   );
